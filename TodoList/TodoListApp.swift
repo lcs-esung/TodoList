cab2b9b113796f70065d0dea88d503bc682e4524
Edna Sung <esung@lcs.on.ca>
2024-04-05 14:59:51 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Edna Sung on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
