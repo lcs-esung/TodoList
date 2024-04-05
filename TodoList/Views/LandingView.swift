9e2f35427d78a8ffbe1514ea64a0c9e5ad3d6885
Edna Sung <esung@lcs.on.ca>
2024-04-05 15:08:01 -0400

    Finish setting up.

---
+++ TodoList/Views/LandingView.swift
@@ -0,0 +1,26 @@
+//
+//  LandingView.swift
+//  TodoList
+//
+//  Created by Edna Sung on 2024-04-05.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    
+    // MARK: Stored Properties
+    
+    
+    
+    
+    
+    // MARK: Computed Properties
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
