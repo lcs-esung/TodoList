0f0febcb44effd0ed5c3190edff3ff5a5c2cce2b
Edna Sung <esung@lcs.on.ca>
2024-04-08 13:55:27 -0400

    Added button to add new to-do item.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -12,6 +12,7 @@ struct LandingView: View {
     // MARK: Stored Properties
     @State var newItemDescription = ""
     
+    // The search text
     @State var searchText = ""
     
     // MARK: Computed Properties
@@ -20,19 +21,31 @@ struct LandingView: View {
         NavigationView {
             
             VStack {
+                
                 List {
+                    
                     Text("Study for Chemistry quiz")
                     Text("Finish Computer science assignment")
                     Text("Go for a run around campus")
+                    
                 }
                 .searchable(text: $searchText)
+                
+                HStack{
+                    TextField("Enter a to-do item", text: $newItemDescription)
+                    
+                    Button("ADD") {
+                        // Add the new to-do item
+                    }
+                  
+                }
+                .padding(20)
+                .navigationTitle("To do")
             }
-            .navigationTitle("To do")
+            
         }
-       
     }
 }
-
 #Preview {
     LandingView()
 }
